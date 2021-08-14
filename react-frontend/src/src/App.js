350a483289eb3008230b1dd08cb4528e90fcc2e2
devarshamin98 <79574249+devarshamin98@users.noreply.github.com>
2021-08-14 18:41:56 -0500

    Add files via upload

---
+++ react-frontend/src/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Predict from './component/Predict';
+
+function App() {
+  return (
+    <div className="App h-100">
+      <Predict />
+    </div>
+  );
+}
+
+export default App;
